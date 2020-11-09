2e6be25e92100b853bb9eda7d17a9a4f1ab0a63e
Marat Maksumov <maksumov@users.noreply.github.com>
2020-11-09 05:20:41 +0300

    feat: create extension uuid_generate_v4()

--- product-service/sql/store.ddl.sql
+++ product-service/sql/store.ddl.sql
@@ -1,9 +1,8 @@
 CREATE TABLE IF NOT EXISTS store.products (
-	id uuid NOT NULL,
+	id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
 	title text NOT NULL,
 	description text NULL,
-	price int4 NOT NULL,
-	CONSTRAINT products_pk PRIMARY KEY (id)
+	price int4 NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS store.stocks (
