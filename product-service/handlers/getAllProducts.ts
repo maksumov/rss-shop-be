8d8a948531890dfe53089d8b341c59b722ac2ab2
Marat Maksumov <maksumov@users.noreply.github.com>
2020-11-09 08:10:47 +0300

    feat: implement task 4.2
    refactor pg-clients.ts
    refactor getAllProducts.ts to get products from db

--- product-service/handlers/getAllProducts.ts
+++ product-service/handlers/getAllProducts.ts
@@ -1,14 +1,23 @@
 import { APIGatewayProxyHandler } from "aws-lambda";
 import "source-map-support/register";
-import getProducts from "./utils/asyncRequest";
 import logger from "./utils/logger";
+import { pgQuery } from "./utils/pg-client";
 import response from "./utils/response";
 
 export const getAllProducts: APIGatewayProxyHandler = async (event) => {
   logger.info(event, "getAllProducts");
 
   try {
-    return response(200, JSON.stringify(await getProducts(), null, 2));
+    return response(
+      200,
+      JSON.stringify(
+        await pgQuery(`SELECT products.*, stocks.count
+    FROM store.products products, store.stocks stocks
+    WHERE stocks.product_id = products.id`),
+        null,
+        2
+      )
+    );
   } catch (err) {
     logger.error(err, "Error catching products list");
     return response(500, "Internal Server Error");
--- product-service/handlers/utils/pg-client.ts
+++ product-service/handlers/utils/pg-client.ts
@@ -1,9 +1,9 @@
-import { Client } from "pg";
+import { Client, ClientConfig } from "pg";
 
 const { PG_HOST, PG_PORT, PG_DATABASE, PG_USERNAME, PG_PASSWORD } = process.env;
-const dbOptions = {
+const dbOptions: ClientConfig = {
   host: PG_HOST,
-  port: PG_PORT,
+  port: parseInt(PG_PORT, 10),
   database: PG_DATABASE,
   user: PG_USERNAME,
   password: PG_PASSWORD,
@@ -13,18 +13,22 @@ const dbOptions = {
   connectionTimeoutMillis: 5000,
 };
 
-const invoke = async (event) => {
+const pgQuery = async (sql: string) => {
   const client = new Client(dbOptions);
   await client.connect();
 
+  let result;
+
   try {
-    const ddlResult = await client.query();
+    result = await client.query(sql);
   } catch (error) {
     console.error("Error during database request executing: ", error);
   } finally {
     // In case if error is occured, connection won't close automatically
     client.end(); // manual closing of connection
   }
+
+  return result.rows;
 };
 
-export { invoke };
+export { pgQuery };
