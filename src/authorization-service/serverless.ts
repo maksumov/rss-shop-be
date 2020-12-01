43efc2f353bc929b6439b0a7eb11613823a1a380
Marat Maksumov <maksumov@users.noreply.github.com>
2020-12-01 09:13:58 +0300

    init: start task 7
    create new authorization service from serverless aws-nodejs-typescript
    template
    link new service to serverless dashboard
    add region & stage to serverless.ts
    add description for cloudfront stack

---
+++ src/authorization-service/serverless.ts
@@ -0,0 +1,51 @@
+import type { Serverless } from "serverless/aws";
+
+const serverlessConfiguration: Serverless = {
+  service: {
+    name: "authorization-service",
+  },
+  org: "maksumov",
+  app: "rss-store",
+  frameworkVersion: "2",
+  custom: {
+    webpack: {
+      webpackConfig: "./webpack.config.js",
+      includeModules: true,
+    },
+  },
+  // Add the serverless-webpack plugin
+  plugins: ["serverless-webpack"],
+  provider: {
+    name: "aws",
+    region: "eu-west-1",
+    stage: "dev",
+    runtime: "nodejs12.x",
+    apiGateway: {
+      minimumCompressionSize: 1024,
+    },
+    environment: {
+      AWS_NODEJS_CONNECTION_REUSE_ENABLED: "1",
+    },
+  },
+
+  resources: {
+    Description: "RS School Node in AWS course - authorization service stack",
+    Resources: {},
+  },
+
+  functions: {
+    hello: {
+      handler: "handler.hello",
+      events: [
+        {
+          http: {
+            method: "get",
+            path: "hello",
+          },
+        },
+      ],
+    },
+  },
+};
+
+module.exports = serverlessConfiguration;
