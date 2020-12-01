157f9b14a723c15ad526af72d065d5305e325c17
Marat Maksumov <maksumov@users.noreply.github.com>
2020-12-01 03:58:43 +0300

    feat: add batch creating products from SQS
    add creating products and corresponding stocks in batch mode
    (lambda function catalogBatchProcess is triggered by SQS)
    add catalog sample in CSV (testCatalog.csv)

--- src/product-service/handlers/catalogBatchProcess.ts
+++ src/product-service/handlers/catalogBatchProcess.ts
@@ -1,7 +1,37 @@
 import { SQSEvent, SQSHandler } from "aws-lambda";
 import logger from "./utils/logger";
+import { pgQuery } from "./utils/pg-client";
 
 export const catalogBatchProcess = (event: SQSEvent): SQSHandler => {
   logger.info(event, "catalogBatchProcessing");
+
+  event.Records.forEach(async (record) => {
+    const { title, description, price, count } = JSON.parse(record.body);
+    // TODO: validate payload
+
+    logger.info({ title, description, price, count }, "catalogBatchProcessing");
+
+    try {
+      await pgQuery({ text: "BEGIN" });
+
+      const res = await pgQuery({
+        name: "add product to products list and get its id",
+        text: `INSERT INTO store.products (title,description,price) VALUES ($1, $2, $3) RETURNING id`,
+        values: [title, description, parseInt(price, 10)],
+      });
+      const { id } = res[0];
+
+      await pgQuery({
+        name: "add stocks for new product",
+        text: `INSERT INTO store.stocks (product_id,count) VALUES ($1, $2)`,
+        values: [id, parseInt(count, 10)],
+      });
+      await pgQuery({ text: "COMMIT" });
+    } catch (err) {
+      await pgQuery({ text: "ROLLBACK" });
+      logger.error(err, "Error creating product");
+    }
+  });
+
   return;
 };
